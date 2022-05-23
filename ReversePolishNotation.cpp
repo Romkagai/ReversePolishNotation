3cf5aa95cd90f51e1a579065d9845ee849eb6e26
Roman Gaisin <106104315+Romkagai@users.noreply.github.com>
2022-05-24 00:18:57 +0700

    Update ReversePolishNotation.cpp

--- ReversePolishNotation.cpp
+++ ReversePolishNotation.cpp
@@ -15,10 +15,10 @@ using namespace std;
 
 using namespace std;
 
-struct Usel         //Структура для работы со стеком
+struct Usel         //Structure for working with stack
 {
-    float key;      //Информационное поле типа !float!
-    Usel* next;     //Указатель на следующий элемент
+    float key;
+    Usel* next;     
 };
 struct Stack
 {
@@ -67,7 +67,7 @@ int Priority(char s)
         case '-': return 2;
         case '*': return 3;
         case '/': return 3;
-        default: cout << "Ошибка! Данная операция не предусмотрена программой!"; exit(0);
+        default: cout << "Error! You can't use this operation!"; exit(0);
     }
 }
 bool isDigit(char ch)
@@ -95,53 +95,52 @@ bool isSign(char ch)
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
     char s[80], p[100], ch;
     Stack sign, prior;
     int i, j, t;
-    cout << "Введите формулу" << endl;
+    cout << "Enter the formula" << endl;
     cin >> s;
 
-    // Пример 5*(12+44)-56*2 = 168
-    // Пример a*(12+44)-b*2 
+    // Example 5*(12+44)-56*2 = 168
+    // Example a*(12+44)-b*2 
 
-    // Шаг 1 - Проверка введенной формулы на ошибки 
-    int bracketCounter = 0;                                             //Счетчик скобок
+    // Step 1 - Checking formula for mistakes 
+    int bracketCounter = 0;                                             //bracketCounter
     for (int i = 0; i < strlen(s); i++) {
-        if (isDigit(s[i]) && (isLetter(s[i + 1]))) { cout << "Ошибка! Два операнда подряд. Проверьте правильность ввода формулы."; exit(0); }
-        if (isLetter(s[i]) && (isLetter(s[i + 1]) || isDigit(s[i+1]))) { cout << "Ошибка! Два операнда подряд. Проверьте правильность ввода формулы."; exit(0); }
-        if (isSign(s[i]) && (isSign(s[i + 1]))) { cout << "Ошибка! Два знака подряд. Проверьте правильность ввода формулы."; exit(0); }
+        if (isDigit(s[i]) && (isLetter(s[i + 1]))) { cout << "Error! Two operands in a row!"; exit(0); }
+        if (isLetter(s[i]) && (isLetter(s[i + 1]) || isDigit(s[i+1]))) { cout << "Error! Two operands in a row!"; exit(0); }
+        if (isSign(s[i]) && (isSign(s[i + 1]))) { cout << "Error! Two signs in a row!"; exit(0); }
         if (s[i] == '(') bracketCounter++;
         if (s[i] == ')') bracketCounter--;
         if (bracketCounter < 0) { 
-            cout << "Ошибка! Потеряна открывающая скобка!"; exit(0);    //Если в какой-то момент счетчик скобок <0 - Ошибка
+            cout << "Error! You've lost an open bracket"; exit(0);    //If bracketCounter < 0
         } 
     }
     if (bracketCounter > 0) { 
-        cout << "Ошибка! Потеряна закрывающая скобка!"; exit(0);        //Если после всего цикла счетчик скобок >0 - Ошибка
+        cout << "Error! You've lost an closing bracket"; exit(0);        //If bracketCounter > 0 
     } 
 
     
 
     
-    // Шаг 2 - Формирование ПОЛИЗ'а
+    // Step 2 - Forming RPN
 
     for (i = j = 0; s[i]; i++)
     {
-        if (isDigit(s[i])) {                                            //Проверка на число
-            while (isDigit(s[i])) { p[j] = s[i]; j++; i++; }            //Пока идут цифры пишем в ПОЛИЗ, как только закончились    
-            p[j] = ' '; j++; i--;                                       //Ставим пробел
+        if (isDigit(s[i])) {                                            //Checking for digit
+            while (isDigit(s[i])) { p[j] = s[i]; j++; i++; }            //While we get number, put them to RPN   
+            p[j] = ' '; j++; i--;                                       //Then put whitespace
         }
-        else if (isLetter(s[i])) {                                      //Проверка на букву
-            p[j] = s[i]; j++;                                           //Если встречаем букву, пишем в ПОЛИЗ
-            p[j] = ' '; j++;                                            //Ставим пробел
+        else if (isLetter(s[i])) {                                      //Checking for letter
+            p[j] = s[i]; j++;                                           //If we get letter, put letter to RPN
+            p[j] = ' '; j++;                                            //Putting whitespace
         }
-        else if (sign.Empty() || s[i] == '('){                          //Если в стеке пусто или '(' заносим знак в стек
+        else if (sign.Empty() || s[i] == '('){                          //If stack is empty or "(" push sign to stack
             sign.Push(s[i]);
             prior.Push(Priority(s[i]));
         }
-        else if (s[i]==')' )                                            //Если знак ')' извлекаем все знаки операций до первой                                                                        
-        {                                                               //встречающейся '(' и заносим в ПОЛИЗ
+        else if (s[i]==')' )                                            //If ")" pop all signs before first "("                                                                        
+        {                                                               //and put them to RPN
             while (sign.Top() != '(')
             {
                 p[j] = sign.Pop();
@@ -170,8 +169,8 @@ int main()
             prior.Push(Priority(s[i]));
         }      
     }
-    while (!sign.Empty())                                               //Если "формула" закончилась, переносим остаток стека  
-    {                                                                   //В ПОЛИЗ
+    while (!sign.Empty())                                               //If formula has ended push stack  
+    {                                                                   //to RPN
         p[j] = sign.Pop();
         p[j + 1] = ' ';
         t = prior.Pop();
@@ -179,44 +178,44 @@ int main()
     }
     p[j-1] = '\0';
 
-    cout << "Формируем ПОЛИЗ:" << endl << p << endl;
+    cout << "Forming RPN:" << endl << p << endl;
     
-    // Шаг 3 - Вычисление ПОЛИЗ'а
+    // Step 3 - RPN calculation
     
     float x, y, result;
     Stack Calculation;
 
-    float numberArray[26] = {};                                             //Создаём массив 0 для переменных от a до z
+    float numberArray[26] = {};                                             //Creating array "0" for letter from a to z
     
 
     for (i = 0; i<strlen(p); )
     {
-        if (isDigit(p[i]))                                                  //Если встречаем цифру
+        if (isDigit(p[i]))                                                  //If we got digit
         {
             float k = 0;
-            for (; p[i] != ' '; i++) k = k * 10 + (p[i] - '0');             //Ищем число до знака пробела           
-            Calculation.Push(k);                                            //Записываем в стек         
+            for (; p[i] != ' '; i++) k = k * 10 + (p[i] - '0');             //Find number before whitespace        
+            Calculation.Push(k);                                            //Push to stack     
         }
-        else if(isLetter(p[i]))                                             //Если встречаем букву
+        else if(isLetter(p[i]))                                             //If we got letter
         {
-            if (numberArray[p[i] - 97] == 0)                                //Если переменная в массиве = 0
+            if (numberArray[p[i] - 97] == 0)                                //If variable in array = 0
             {
-                cout << "Введите значение переменной " << p[i] << ": ";     //Просим значение
+                cout << "Enter variable " << p[i] << ": ";     //Ask for variable 
                 cin >> numberArray[p[i] - 97];
             }                   
-            Calculation.Push(numberArray[p[i] - 97]); i++;                  //Записываем в стек 
+            Calculation.Push(numberArray[p[i] - 97]); i++;                  //Push to stack
         }
-        else if (p[i])                                                      //Если встречаем операцию
+        else if (p[i])                                                      //If we got operation sign
         {
-            y = Calculation.Pop(); x = Calculation.Pop();                   //Достаем из стека два верхних элемента
-            switch (p[i])                                                   //Проводим операцию
+            y = Calculation.Pop(); x = Calculation.Pop();                   //Pop from stack two top elements
+            switch (p[i])                                                   //Get calculations
             {
             case '+': result = x + y; break;
             case '-': result = x - y; break;
             case '*': result = x * y; break;
             case '/': result = x / y; break;
             }
-            Calculation.Push(result); i++;                                  //Пишем результат обратно в стек
+            Calculation.Push(result); i++;                                  //Writing a result back to stack
         }
         i++;
     } 
